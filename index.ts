f42b1fc5b4ebceb0c9a746ed845acb355f88fec7
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-30 09:03:04 +0700

    Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from 'dotenv';
 import * as database from "./config/database";
+import Topic from "./models/topic.model";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
@@ -12,7 +13,11 @@ app.set("view engine", "pug");
 // connect database
 database.connect();
 
-app.get("/topics",(req: Request, res: Response)=> {
+app.get("/topics", async (req: Request, res: Response)=> {
+    const topics = await Topic.find({
+        deleted: false
+    })
+    console.log(topics);
     res.render("client/pages/topics/index.pug");
 })
 app.listen(port, ()=> {
