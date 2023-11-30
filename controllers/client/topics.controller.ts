5559ffa233d327cb2c37e5ced0afb2bffd6012aa
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-30 09:42:49 +0700

    Cấu trúc lại folder PUG

--- controllers/client/topics.controller.ts
+++ controllers/client/topics.controller.ts
@@ -6,5 +6,8 @@ export const topics = async (req: Request, res: Response) => {
         deleted: false
     })
     console.log(topics);
-    res.render("client/pages/topics/index.pug");
+    res.render("client/pages/topics/index.pug", {
+        pageTitle: 'Chủ đề bài hát'
+    }
+    );
 } 
