08ec86cd4e03ae52cb37a8a049f7b52a76f59e8b
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-12-01 17:48:00 +0700

    Làm nút bài hát yêu thích trang client

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -2,6 +2,7 @@ import { Response, Request } from "express";
 import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
+import FavoriteSong from "../../models/favoriteSong.model";
 
 //[GET]/songs/list/:slugTopic
 export const list = async (req: Request, res: Response) => {
@@ -52,7 +53,10 @@ export const detail = async (req: Request, res: Response) => {
         _id: song.topicId,
         deleted: false
     }).select("title");
-
+    const favoriteSong = await FavoriteSong.findOne({
+        songId : song.id
+    })
+    song["isFavoriteSong"] = favoriteSong ? true: false
 
     res.render("client/pages/songs/detail", {
         pageTitle: "Chi tiết bài hát",
@@ -84,5 +88,35 @@ export const like = async(req: Request, res: Response) => {
         code: 200,
         message: 'Thành công!',
         like: newLike
+    });
+}
+// [PATCH] /songs/favorite/:typeFavorite/:idSong
+export const favorite = async (req: Request, res: Response) => {
+    const idSong: string = req.params.idSong;
+    const typeFavorite: string = req.params.typeFavorite;
+
+    switch (typeFavorite) {
+        case "favorite":
+            const existFavoriteSong = await FavoriteSong.findOne({
+                songId: idSong
+            });
+            if(!existFavoriteSong) {
+                const record = new FavoriteSong({
+                    songId: idSong
+                })
+                await record.save()
+            }
+            break;
+        case "unFavorite":
+            await FavoriteSong.deleteOne({
+                songId: idSong
+            })
+            break;
+        default:
+            break;
+    }
+    res.json({
+        code: 200,
+        message: "Thành công!"
     });
 }
