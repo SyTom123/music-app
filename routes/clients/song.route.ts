ef6acd08154ffc5523fd89352ec5966defbb5f86
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-12-01 16:37:04 +0700

    Làm tính năng like

--- routes/clients/song.route.ts
+++ routes/clients/song.route.ts
@@ -2,5 +2,9 @@ import {  Router } from "express";
 import * as controller from "../../controllers/client/song.controller"
 const router: Router = Router();
 router.get("/:slugTopic", controller.list );
+
 router.get("/detail/:slugSong", controller.detail);
+
+router.patch("/like/:typeLike/:idSong", controller.like);
+
 export default router;
