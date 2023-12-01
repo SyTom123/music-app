e7e2e9d5e9b2bc080499fa98c64932c4b320100d
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-12-01 16:01:40 +0700

    Làm trang chi tiết bài hát

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -3,6 +3,7 @@ import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 
+//[GET]/songs/list/:slugTopic
 export const list = async (req: Request, res: Response) => {
     const slug = req.params.slugTopic;
     
@@ -32,3 +33,31 @@ export const list = async (req: Request, res: Response) => {
         songs: songs
     });
 }
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+    const slugSong: string = req.params.slugSong;
+    
+    const song = await Song.findOne({
+        slug: slugSong,
+        status: "active",
+        deleted: false
+    });
+    const singer = await Singer.findOne({
+        _id: song.singerId,
+        deleted: false
+    }).select("fullName");
+
+    const topic = await Topic.findOne({
+        _id: song.topicId,
+        deleted: false
+    }).select("title");
+
+
+    res.render("client/pages/songs/detail", {
+        pageTitle: "Chi tiết bài hát",
+        song: song,
+        singer: singer,
+        topic: topic
+    });
+  };
--- routes/clients/song.route.ts
+++ routes/clients/song.route.ts
@@ -2,4 +2,5 @@ import {  Router } from "express";
 import * as controller from "../../controllers/client/song.controller"
 const router: Router = Router();
 router.get("/:slugTopic", controller.list );
+router.get("/detail/:slugSong", controller.detail);
 export default router;
